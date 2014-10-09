80bd0efd273e15e06a2e0aafe9f80a75759ed358
IsaWartke <isa.wartke@gmx.de>
2014-10-09 10:02:47 +0200

    Initialer Commit

---
+++ ASCII Dungeon/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASCII_Dungeon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
