3e491533618f654dffa5865a4d8c6f987294b35b
MrCkurab <magbrauckhoff@aol.com>
2014-10-09 11:03:03 +0200

    Erster Build
    Grundlegender Build, kann erste Räume generieren.

---
+++ ASCII Dungeon/Map.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public class Map
+    {
+        private readonly Tile[,] tiles;
+
+        public Map(int width, int height)
+        {
+            tiles = new Tile[width, height];
+            for (int x = 0; x <= tiles.GetUpperBound(0); x++)
+            {
+                for (int y = 0; y <= tiles.GetUpperBound(1); y++)
+                {
+                    tiles[x, y] = new Tile(x, y, this);
+                }
+            }
+        
+        }
+
+        public void Render()
+        {
+            for (int x = 0; x <= tiles.GetUpperBound(0); x++)
+            {
+                for (int y = 0; y <= tiles.GetUpperBound(1); y++)
+                {
+                    Console.SetCursorPosition(x, y);
+                    tiles[x, y].Render();
+                }
+            }
+        }
+
+        public Tile GetTile(int x, int y)
+        {
+            return tiles[x, y];
+        }
+
+        public void NextRound()
+        {
+            for (int x = 0; x <= tiles.GetUpperBound(0); x++)
+            {
+                for (int y = 0; y <= tiles.GetUpperBound(1); y++)
+                {
+                    tiles[x, y].NextRound();
+                }
+            }
+        }
+
+        public void Flush()
+        {
+            for (int x = 0; x <= tiles.GetUpperBound(0); x++)
+            {
+                for (int y = 0; y <= tiles.GetUpperBound(1); y++)
+                {
+                    tiles[x, y].Flush();
+                }
+            }
+        }
+
+        public void InitLvl()
+        {
+            for (int x = 0; x <= tiles.GetUpperBound(0); x++)
+            {
+                for (int y = 0; y <= tiles.GetUpperBound(1); y++)
+                {
+                    if (x == 0 || y == 0 ||
+                        x == tiles.GetUpperBound(0) || y == tiles.GetUpperBound(1))
+                    {
+                        tiles[x, y].State = 1;
+                    }
+                    else if (x == tiles.GetUpperBound(0) / 2 && y == tiles.GetUpperBound(1) / 2)
+                    {
+                        tiles[x, y].State = 2;
+                    }
+                    else
+                    {
+                        tiles[x, y].State = 0;
+                    }
+                }
+            }
+        }
+    }
+}
--- ASCII Dungeon/Program.cs
+++ ASCII Dungeon/Program.cs
@@ -2,14 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace ASCII_Dungeon
+
+namespace ConsoleApplication1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.CursorVisible = false;
+            Map map = new Map(9, 9);
+
+            map.InitLvl();
+            map.Render();
+                map.NextRound();
+                map.Flush();
+                Console.ReadKey();
+
         }
     }
 }
