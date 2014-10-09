d8bdff9d273be594f0a0b7831822904371005f95
IsaWartke <isa.wartke@gmx.de>
2014-10-09 11:31:25 +0200

    Map eingebunden.

--- ASCII Dungeon/Program.cs
+++ ASCII Dungeon/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 
 namespace ConsoleApplication1
@@ -11,13 +12,21 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.CursorVisible = false;
-            Map map = new Map(9, 9);
+            //Map map = new Map(9, 9);
 
-            map.InitLvl();
-            map.Render();
-                map.NextRound();
-                map.Flush();
-                Console.ReadKey();
+            //map.InitLvl();
+            //map.Render();
+            //    map.NextRound();
+            //    map.Flush();
+
+          string[] lines = File.ReadAllLines("Map.txt");
+
+          for (int i = 0; i < lines.Length; i++ )
+          {
+              Console.WriteLine(lines[i]);
+          }
+
+              Console.ReadKey();
 
         }
     }
