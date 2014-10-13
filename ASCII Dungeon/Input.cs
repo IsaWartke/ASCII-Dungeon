153ea05cf252cf29092cefb19af4873fa1258dca
MrCkurab <magbrauckhoff@aol.com>
2014-10-13 14:32:52 +0200

    Input geupdated, mit GameControll verknüpft, Render schleife an Tasteneingabe gebunden.

--- ASCII Dungeon/Input.cs
+++ ASCII Dungeon/Input.cs
@@ -17,5 +17,48 @@ namespace ASCII_Dungeon
         public event InputEvent OnRightKey;
         public event InputEvent OnSpaceKey;
 
+        public bool KeyStroke()
+        {
+            if (Console.KeyAvailable)
+            {
+                if (OnUpKey != null)
+                {
+                    OnUpKey.Invoke(Console.ReadKey());            
+                }
+
+                if (OnDownKey != null)
+                {
+                    OnDownKey.Invoke(Console.ReadKey());
+                }
+
+                if (OnLeftKey != null)
+                {
+                    OnLeftKey.Invoke(Console.ReadKey());
+                }
+
+                if (OnRightKey != null)
+                {
+                    OnRightKey.Invoke(Console.ReadKey());
+                }
+
+                if (OnSpaceKey != null)
+                {
+                    OnSpaceKey.Invoke(Console.ReadKey());
+                }
+                return true;                
+                    
+            }
+            return false;
+        }
+
+        /*
+        Input.OnUpKey += Player.UpKey;
+        Input.OnDownKey += Player.DownKey;
+        Input.OnLeftKey += Player.LeftKey;
+        Input.OnRightKey += Player.RightKey;
+        Input.OnSpaceKey += Player.SpaceKey;
+        
+        
+        */
     }
 }
