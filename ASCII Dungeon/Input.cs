3752e4ad2b853a86279b9b8f8bf08ac53dfb3fb9
MrCkurab <magbrauckhoff@aol.com>
2014-10-13 13:37:12 +0200

    Input Klasse erster Entwurf.

---
+++ ASCII Dungeon/Input.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASCII_Dungeon
+{
+    class Input
+    {
+
+        public delegate void InputEvent(ConsoleKeyInfo key);
+
+        public event InputEvent OnUpKey;
+        public event InputEvent OnDownKey;
+        public event InputEvent OnLeftKey;
+        public event InputEvent OnRightKey;
+        public event InputEvent OnSpaceKey;
+
+    }
+}
