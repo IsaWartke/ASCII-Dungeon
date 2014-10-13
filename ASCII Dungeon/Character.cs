f51f5c5cad1f97af450020fc575dfce646dc7f0b
CarryKey <carina.t@darkkey.de>
2014-10-13 14:10:55 +0200

    Neuaufbau nach UML

---
+++ ASCII Dungeon/Character.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASCII_Dungeon
+{
+    abstract class Character
+    {
+        protected char[] Appearance = new char[4];
+        protected byte LifePoints;
+        protected byte AttackPoints;
+        protected char ViewingDirection;
+        protected byte Xpos;
+        protected byte Ypos;
+
+
+
+        protected abstract void Move();
+
+        protected void IsAttacked(byte attack_points) { }
+
+    }
+}
